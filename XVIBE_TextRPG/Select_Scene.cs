c8ce576d716327a56c4cca30e2ea3dac30e3cc52
jkh201020 <jack0517@naver.com>
2025-04-21 17:05:22 +0900

    fix: Select_Scene 테스트로 사용한 코드 제거
    Select_Scene 테스트로 사용한 코드 제거

--- XVIBE_TextRPG/Select_Scene.cs
+++ XVIBE_TextRPG/Select_Scene.cs
@@ -30,78 +30,12 @@
                 {
                     case "1": // 1번 누를 시
                         loop = false; // 반복 종료
-                        test1(); // 상태 보기 창으로 이동 (임의로 이름 붙임)
+                        //test1(); // 상태 보기 창으로 이동 (임의로 이름 붙임)
                         break;
 
                     case "2": // 2번 누를 시
                         loop = false; // 반복 종료
-                        test2(); // 전투 시작 창으로 이동 (임의로 이름 붙임)
-                        break;
-
-                    default: // 나머지 입력
-                        Message("잘못된 입력입니다 ");
-                        break;
-                }
-            } while (loop);
-        }
-
-        public void test1() // 1. 게임 시작 화면
-        {
-            bool loop = true; // 반복
-
-            do
-            {
-                Console.Clear(); // 화면 초기화
-
-                Console.WriteLine("스파르타 던전에 오신 여러분 환영합니다.\n이제 전투를 시작할 수 있습니다.\n"); // 환영 문구
-                Console.WriteLine("1. Scene\n2. test2\n"); // 행동 선택
-                Console.Write("원하시는 행동을 입력해주세요.\n>> "); // 입력 문구
-
-                string input = Console.ReadLine(); // 입력 받기
-
-                switch (input)
-                {
-                    case "1": // 1번 누를 시
-                        loop = false; // 반복 종료
-                        Scene(); // 상태 보기 창으로 이동 (임의로 이름 붙임)
-                        break;
-
-                    case "2": // 2번 누를 시
-                        loop = false; // 반복 종료
-                        test2(); // 전투 시작 창으로 이동 (임의로 이름 붙임)
-                        break;
-
-                    default: // 나머지 입력
-                        Message("잘못된 입력입니다 ");
-                        break;
-                }
-            } while (loop);
-        }
-
-        public void test2() // 1. 게임 시작 화면
-        {
-            bool loop = true; // 반복
-
-            do
-            {
-                Console.Clear(); // 화면 초기화
-
-                Console.WriteLine("스파르타 던전에 오신 여러분 환영합니다.\n이제 전투를 시작할 수 있습니다.\n"); // 환영 문구
-                Console.WriteLine("1. Scene\n2. test1\n"); // 행동 선택
-                Console.Write("원하시는 행동을 입력해주세요.\n>> "); // 입력 문구
-
-                string input = Console.ReadLine(); // 입력 받기
-
-                switch (input)
-                {
-                    case "1": // 1번 누를 시
-                        loop = false; // 반복 종료
-                        Scene(); // 상태 보기 창으로 이동 (임의로 이름 붙임)
-                        break;
-
-                    case "2": // 2번 누를 시
-                        loop = false; // 반복 종료
-                        test1(); // 전투 시작 창으로 이동 (임의로 이름 붙임)
+                        //test2(); // 전투 시작 창으로 이동 (임의로 이름 붙임)
                         break;
 
                     default: // 나머지 입력
