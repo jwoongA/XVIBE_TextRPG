1b6e8dfa35da63c2d621a8231e59666a98b046db
Lilyvly <dlgurtn0917@gmail.com>
2025-04-23 11:25:46 +0900

    add shop, inventory system
    인벤토리와 상점 시스템 구현

--- XVIBE_TextRPG/Equipment.cs
+++ XVIBE_TextRPG/Equipment.cs
@@ -1,29 +1,83 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace XVIBE_TextRPG
+﻿namespace XVIBE_TextRPG
 {
-    internal class Equipment//장비는 아직 구현이 안됨 착용 장비에 맞춰 ATK, DEF 보너스가 추가됨
+    internal class Equipment
     {
-        public static int ATKBonus { get; set; } = 0; // 공격력 보너스
-        public static int DEFBonus { get; set; } = 0; // 방어력 보너스
-        //장비의 보유량, 상점 재고 등의 int 변수 추가 예정
-        //무기 착용 여부, 방어구 착용 여부 등의 bool 변수 추가 예정
+        public static int ATKBonus { get; set; } = 0;
+        public static int DEFBonus { get; set; } = 0;
+        
+        public enum WeaponType { Sword, Dagger, Staff }
 
-        // 무기의 종류를 enum으로 정의
-        public enum WeaponType
+        public class Weapon
         {
-            Sword,  // 무기
-            Dagger, // 단검
-            Staff,  // 지팡이
+            public string Name { get; }
+            public WeaponType Type { get; }
+            public int ATK { get; }
+            public int Price { get; }
+
+            public Weapon(string name, WeaponType type, int atk, int price)
+            {
+                Name = name;
+                Type = type;
+                ATK = atk;
+                Price = price;
+            }
+
+            public override string ToString()
+            {
+                return $"{Name} (공격력: {ATK}, 가격: {Price}G)";
+            }
         }
 
+        public static List<Weapon> Inventory = new List<Weapon>();
+        public static Weapon EquippedWeapon = null;
 
-        // 장비 목록을 스태틱 리스트로 관리 예정
-        // 장비를 착용하는 메서드, 장비를 해제하는 메서드, 상점 재고를 관리하는 메서드 등을 추가할 예정
+        public static void Equip(Weapon weapon)
+        {
+            EquippedWeapon = weapon;
+            ATKBonus = weapon.ATK;
+            Console.WriteLine($"{weapon.Name}을(를) 장착했습니다!");
+        }
 
+        public static void Unequip()
+        {
+            if (EquippedWeapon != null)
+            {
+                Console.WriteLine($"{EquippedWeapon.Name}을(를) 해제했습니다!");
+                EquippedWeapon = null;
+                ATKBonus = 0;
+            }
+            else Console.WriteLine("장착된 무기가 없습니다.");
+        }
+
+        public static void ShowInventory()
+        {
+            Console.WriteLine("===== 인벤토리 =====");
+            if (Inventory.Count == 0)
+            {
+                Console.WriteLine("보유한 무기가 없습니다.");
+            }
+            else
+            {
+                for (int i = 0; i < Inventory.Count; i++)
+                {
+                    var w = Inventory[i];
+                    Console.WriteLine($"{i + 1}. {w}");
+                }
+            }
+
+            Console.WriteLine("0. 뒤로가기");
+            Console.WriteLine("장착할 무기 번호를 입력하거나 해제하려면 U를 입력하세요.");
+            Console.Write(">> ");
+            var input = Console.ReadLine();
+
+            if (input == "U")
+            {
+                Unequip();
+            }
+            else if (int.TryParse(input, out int index) && index > 0 && index <= Inventory.Count)
+            {
+                Equip(Inventory[index - 1]);
+            }
+        }
     }
 }
